0ee7bb03c3e4771a1e8c2d7a2efc921dab8b7c2c
creeper123123321 <creeper123123321@gmail.com>
2019-04-16 17:18:29 -0300

    Fix server mixin

--- src/main/java/com/github/creeper123123321/viafabric/mixin/MixinServerNetworkIoChInit.java
+++ src/main/java/com/github/creeper123123321/viafabric/mixin/MixinServerNetworkIoChInit.java
@@ -37,8 +37,8 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import us.myles.ViaVersion.api.data.UserConnection;
 import us.myles.ViaVersion.api.protocol.ProtocolPipeline;
 
-@Mixin(targets = "net.minecraft.server.ServerNetworkIO$1")
-public class MixinServerNetworkIOChInit {
+@Mixin(targets = "net.minecraft.server.ServerNetworkIo$1")
+public class MixinServerNetworkIoChInit {
     @Inject(method = "initChannel(Lio/netty/channel/Channel;)V", at = @At(value = "TAIL"), remap = false)
     private void onInitChannel(Channel channel, CallbackInfo ci) {
         if (channel instanceof SocketChannel) {
