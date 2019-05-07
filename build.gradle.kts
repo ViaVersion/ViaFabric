8ecb5b649f3a1d79b2227986e64a7f2a009e5d57
creeper123123321 <creeper123123321@gmail.com>
2019-05-07 14:30:10 -0300

    update viaversion

--- build.gradle.kts
+++ build.gradle.kts
@@ -46,8 +46,8 @@ tasks.named<ProcessResources>("processResources") {
 
 dependencies {
     // transitive = false, viabackwards-core because Guava is conflicting on runClient
-    compile("us.myles:viaversion:2.0.2-SNAPSHOT") { isTransitive = false }
-    include("us.myles:viaversion:2.0.2-SNAPSHOT")
+    compile("us.myles:viaversion:2.1.0-1.14.1-pre1") { isTransitive = false }
+    include("us.myles:viaversion:2.1.0-1.14.1-pre1")
     compile("de.gerrygames:viarewind-core:1.4.0") { isTransitive = false }
     include("de.gerrygames:viarewind-core:1.4.0")
     compile("nl.matsv:viabackwards-core:3.0.0-19w11b") { isTransitive = false } // todo update
