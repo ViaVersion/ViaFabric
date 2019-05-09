f909398f38883bf2f283ee3fd4e067611bdec60a
creeper123123321 <creeper123123321@gmail.com>
2019-05-09 13:44:19 -0300

    viaversion 1.14.1 Pre-Release 2, update loader and yarn

--- build.gradle.kts
+++ build.gradle.kts
@@ -46,8 +46,8 @@ tasks.named<ProcessResources>("processResources") {
 
 dependencies {
     // transitive = false, viabackwards-core because Guava is conflicting on runClient
-    compile("us.myles:viaversion:2.1.0-1.14.1-pre1") { isTransitive = false }
-    include("us.myles:viaversion:2.1.0-1.14.1-pre1")
+    compile("us.myles:viaversion:2.1.0-1.14.1-pre2") { isTransitive = false }
+    include("us.myles:viaversion:2.1.0-1.14.1-pre2")
     compile("de.gerrygames:viarewind-core:1.4.0") { isTransitive = false }
     include("de.gerrygames:viarewind-core:1.4.0")
     compile("nl.matsv:viabackwards-core:3.0.0-19w11b") { isTransitive = false } // todo update
@@ -57,7 +57,7 @@ dependencies {
 
     minecraft("com.mojang:minecraft:1.14")
     mappings("net.fabricmc:yarn:1.14+build.8")
-    modCompile("net.fabricmc:fabric-loader:0.4.6+build.141")
+    modCompile("net.fabricmc:fabric-loader:0.4.6+build.143")
 
     modCompile("net.fabricmc:fabric:0.2.7+build.127")
 
