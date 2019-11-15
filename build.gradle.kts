ce15ae5685cfb8093d51f762901eb6a7e20569bf
creeper123123321 <7974274+creeper123123321@users.noreply.github.com>
2019-11-15 12:07:10 -0300

    update gradlew, loom and downgrade minecraft to 1.14.4

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ import org.apache.tools.ant.filters.ReplaceTokens
 plugins {
     id("java")
     id("net.minecrell.licenser") version "0.4.1"
-    id("fabric-loom") version "0.2.4-SNAPSHOT"
+    id("fabric-loom") version "0.2.6-SNAPSHOT"
     id("com.palantir.git-version") version "0.12.0-rc2"
 }
 
@@ -51,11 +51,12 @@ dependencies {
 
     compileOnly("com.google.code.findbugs:jsr305:3.0.2")
 
-    minecraft("com.mojang:minecraft:19w42a")
-    mappings("net.fabricmc:yarn:19w42a+build.1")
-    modCompile("net.fabricmc:fabric-loader:0.6.3+build.167")
+    // Use release, probably intermediary will make it work on snapshots
+    minecraft("com.mojang:minecraft:1.14.4")
+    mappings("net.fabricmc:yarn:1.14.4+build.15")
+    modCompile("net.fabricmc:fabric-loader:0.6.4+build.169")
 
-    modCompile("net.fabricmc.fabric-api:fabric-api:0.4.8+build.255-1.15")
+    modCompile("net.fabricmc.fabric-api:fabric-api:0.4.1+build.245-1.14")
 
     modCompile("io.github.cottonmc:cotton-client-commands:0.4.2+1.14.3-SNAPSHOT")
     include("io.github.cottonmc:cotton-client-commands:0.4.2+1.14.3-SNAPSHOT")
