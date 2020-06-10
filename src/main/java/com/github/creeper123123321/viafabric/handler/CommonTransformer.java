3eda8f31e0368ce9a67445587cccd81a0187f911
KennyTV <kennytv@t-online.de>
2020-06-10 09:18:25 +0200

    Update codec handlers

--- src/main/java/com/github/creeper123123321/viafabric/handler/CommonTransformer.java
+++ src/main/java/com/github/creeper123123321/viafabric/handler/CommonTransformer.java
@@ -24,62 +24,7 @@
 
 package com.github.creeper123123321.viafabric.handler;
 
-import io.netty.buffer.ByteBuf;
-import us.myles.ViaVersion.api.PacketWrapper;
-import us.myles.ViaVersion.api.data.UserConnection;
-import us.myles.ViaVersion.api.type.Type;
-import us.myles.ViaVersion.exception.CancelException;
-import us.myles.ViaVersion.packets.Direction;
-import us.myles.ViaVersion.protocols.base.ProtocolInfo;
-
-import java.util.function.Function;
-
-// TODO delete this when https://github.com/ViaVersion/ViaVersion/pull/1505 is merged
 public class CommonTransformer {
     public static final String HANDLER_DECODER_NAME = "via-decoder";
     public static final String HANDLER_ENCODER_NAME = "via-encoder";
-
-    public static boolean preServerboundCheck(UserConnection user) {
-        // Ignore if pending disconnect
-        if (user.isPendingDisconnect()) return true;
-        // Increment received + Check PPS
-        return user.incrementReceived() && user.handlePPS();
-    }
-
-    public static void preClientbound(UserConnection user) {
-        user.incrementSent();
-    }
-
-    public static boolean willTransformPacket(UserConnection user) {
-        return user.isActive();
-    }
-
-    public static void transformClientbound(ByteBuf draft, UserConnection user, Function<Throwable, Exception> cancelSupplier) throws Exception {
-        if (!draft.isReadable()) return;
-        transform(draft, user, Direction.OUTGOING, cancelSupplier);
-    }
-
-    public static void transformServerbound(ByteBuf draft, UserConnection user, Function<Throwable, Exception> cancelSupplier) throws Exception {
-        if (!draft.isReadable()) return;
-        transform(draft, user, Direction.INCOMING, cancelSupplier);
-    }
-
-    private static void transform(ByteBuf draft, UserConnection user, Direction direction, Function<Throwable, Exception> cancelSupplier) throws Exception {
-        int id = Type.VAR_INT.read(draft);
-        if (id == PacketWrapper.PASSTHROUGH_ID) return;
-        PacketWrapper wrapper = new PacketWrapper(id, draft, user);
-        ProtocolInfo protInfo = user.get(ProtocolInfo.class);
-        try {
-            protInfo.getPipeline().transform(direction, protInfo.getState(), wrapper);
-        } catch (CancelException ex) {
-            throw cancelSupplier.apply(ex);
-        }
-        ByteBuf transformed = draft.alloc().buffer();
-        try {
-            wrapper.writeToBuffer(transformed);
-            draft.clear().writeBytes(transformed);
-        } finally {
-            transformed.release();
-        }
-    }
 }
