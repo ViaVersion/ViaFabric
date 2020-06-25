c56e69fd1ad68bf8d0b20a44cd0b3159d2722726
creeper123123321 <7974274+creeper123123321@users.noreply.github.com>
2020-06-25 16:57:55 -0300

    update gradlew and workaround building

--- src/main/java/com/github/creeper123123321/viafabric/platform/VRConnectionManager.java
+++ src/main/java/com/github/creeper123123321/viafabric/platform/VRConnectionManager.java
@@ -28,7 +28,7 @@ import us.myles.ViaVersion.api.data.UserConnection;
 import us.myles.ViaVersion.api.platform.ViaConnectionManager;
 
 public class VRConnectionManager extends ViaConnectionManager {
-    @Override
+    //@Override TODO: check why Gradle isn't downloading the latest snapshot
     public boolean isFrontEnd(UserConnection connection) {
         return !(connection instanceof VRClientSideUserConnection);
     }
