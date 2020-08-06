158613c87bfe1d8571c82568748beda99f387e9c
creeper123123321 <7974274+creeper123123321@users.noreply.github.com>
2020-08-06 10:44:59 -0300

    bundle viaversion and snakeyaml ViaVersion/ViaVersion#1959

--- build.gradle.kts
+++ build.gradle.kts
@@ -58,7 +58,9 @@ tasks.named<ProcessResources>("processResources") {
 
 dependencies {
     // transitive = false because Guava is conflicting on runClient
-    implementation("us.myles:viaversion:3.1.0-1.16.2-pre1") { isTransitive = false }
+    implementation("us.myles:viaversion:3.1.0-1.16.2-pre2") { isTransitive = false }
+    include("us.myles:viaversion:3.1.0-1.16.2-pre2")
+    include("org.yaml:snakeyaml:1.26")
 
     // Use 1.14.4 release, probably intermediary will make it work on snapshots
     // https://modmuss50.me/fabric.html?&version=1.14.4
