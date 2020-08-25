2368e73d69f92785470edd993c010f9f8bb038c5
creeper123123321 <7974274+creeper123123321@users.noreply.github.com>
2020-08-25 09:35:40 -0300

    Daemon thread, implement per server version fixes #45 and implements #34

---
+++ src/main/java/com/github/creeper123123321/viafabric/ProtocolViaFabricHostname.java
@@ -0,0 +1,53 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2018- creeper123123321 <https://creeper123123321.keybase.pub/>
+ * Copyright (c) 2019- contributors <https://github.com/ViaVersion/ViaFabric/graphs/contributors>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package com.github.creeper123123321.viafabric;
+
+import us.myles.ViaVersion.api.PacketWrapper;
+import us.myles.ViaVersion.api.protocol.SimpleProtocol;
+import us.myles.ViaVersion.api.remapper.PacketRemapper;
+import us.myles.ViaVersion.api.remapper.ValueTransformer;
+import us.myles.ViaVersion.api.type.Type;
+import us.myles.ViaVersion.packets.State;
+
+public class ProtocolViaFabricHostname extends SimpleProtocol {
+    public static final ProtocolViaFabricHostname INSTANCE = new ProtocolViaFabricHostname();
+
+    @Override
+    protected void registerPackets() {
+        registerIncoming(State.HANDSHAKE, 0, 0, new PacketRemapper() {
+            @Override
+            public void registerMap() {
+                map(Type.VAR_INT); // Protocol version
+                map(Type.STRING, new ValueTransformer<String, String>(Type.STRING) {
+                    @Override
+                    public String transform(PacketWrapper packetWrapper, String s) {
+                        return new ViaFabricAddress().parse(s).realAddress;
+                    }
+                });
+            }
+        });
+    }
+}
--- src/main/java/com/github/creeper123123321/viafabric/mixin/client/MixinServerPinger.java
+++ src/main/java/com/github/creeper123123321/viafabric/mixin/client/MixinServerPinger.java
@@ -25,30 +25,26 @@
 
 package com.github.creeper123123321.viafabric.mixin.client;
 
-import com.github.creeper123123321.viafabric.ProtocolViaFabricHostname;
-import com.github.creeper123123321.viafabric.handler.CommonTransformer;
-import com.github.creeper123123321.viafabric.handler.clientside.VRDecodeHandler;
-import com.github.creeper123123321.viafabric.handler.clientside.VREncodeHandler;
-import com.github.creeper123123321.viafabric.platform.VRClientSideUserConnection;
-import io.netty.channel.Channel;
-import io.netty.channel.socket.SocketChannel;
+import com.github.creeper123123321.viafabric.ViaFabricAddress;
+import net.minecraft.client.network.MultiplayerServerListPinger;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import us.myles.ViaVersion.api.data.UserConnection;
-import us.myles.ViaVersion.api.protocol.ProtocolPipeline;
+import org.spongepowered.asm.mixin.injection.Redirect;
 
-@Mixin(targets = "net.minecraft.network.ClientConnection$5")
-public class MixinClientConnectionChInit {
-    @Inject(method = "initChannel", at = @At(value = "TAIL"), remap = false)
-    private void onInitChannel(Channel channel, CallbackInfo ci) {
-        if (channel instanceof SocketChannel) {
-            UserConnection user = new VRClientSideUserConnection(channel);
-            new ProtocolPipeline(user).add(ProtocolViaFabricHostname.INSTANCE);
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 
-            channel.pipeline().addBefore("encoder", CommonTransformer.HANDLER_ENCODER_NAME, new VREncodeHandler(user));
-            channel.pipeline().addBefore("decoder", CommonTransformer.HANDLER_DECODER_NAME, new VRDecodeHandler(user));
+@Mixin(MultiplayerServerListPinger.class)
+public class MixinServerPinger {
+    @Redirect(method = "add", at = @At(value = "INVOKE",
+            target = "Ljava/net/InetAddress;getByName(Ljava/lang/String;)Ljava/net/InetAddress;"))
+    private InetAddress resolveViaFabricAddr(String address) throws UnknownHostException {
+        ViaFabricAddress viaAddr = new ViaFabricAddress().parse(address);
+        if (viaAddr.viaSuffix == null) {
+            return InetAddress.getByName(address);
         }
+
+        InetAddress resolved = InetAddress.getByName(viaAddr.realAddress);
+        return InetAddress.getByAddress(resolved.getHostName() + "." + viaAddr.viaSuffix, resolved.getAddress());
     }
 }
