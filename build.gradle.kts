7a74f411a40f117c50fe1bf594cdcef8773ec5f8
creeper123123321 <7974274+creeper123123321@users.noreply.github.com>
2020-11-14 17:07:23 -0300

    #78 snakeyaml

--- build.gradle.kts
+++ build.gradle.kts
@@ -61,6 +61,7 @@ dependencies {
     // transitive = false because Guava is conflicting on runClient
     implementation("us.myles:viaversion:3.2.0") { isTransitive = false }
     include("us.myles:viaversion:3.2.0")
+    implementation("org.yaml:snakeyaml:1.26")
     include("org.yaml:snakeyaml:1.26")
 
     // Use 1.16 snapshot, probably intermediary will make it work on further versions
