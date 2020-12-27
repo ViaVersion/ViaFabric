d25a095e9ebf831663e1c34db4ee9c33efbf5022
creeper123123321 <7974274+creeper123123321@users.noreply.github.com>
2020-12-27 13:28:48 -0300

    update readme

--- compile_push_branches.sh
+++ compile_push_branches.sh
@@ -3,4 +3,5 @@ git checkout mc-1.8 && ./gradlew clean build \
 && git checkout mc-1.14 && ./gradlew clean build \
 && git checkout mc-1.15 && ./gradlew clean build \
 && git checkout mc-1.16 && ./gradlew clean build \
-&& git push origin mc-1.8 mc-1.14 mc-1.15 mc-1.16
+&& git checkout mc-1.17 && ./gradlew clean build \
+&& git push origin mc-1.8 mc-1.14 mc-1.15 mc-1.16 mc-1.17
