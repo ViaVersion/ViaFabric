5845cd5da39d88bbc75cf0aaf85a238c90606d5f
creeper123123321 <7974274+creeper123123321@users.noreply.github.com>
2021-01-11 12:02:03 -0300

    update readme, parse .viafabric before autodetect, close #89 by stealing viaversion-velocity code

--- src/main/java/com/github/creeper123123321/viafabric/handler/FabricDecodeHandler.java
+++ src/main/java/com/github/creeper123123321/viafabric/handler/FabricDecodeHandler.java
@@ -34,11 +34,14 @@ import us.myles.ViaVersion.exception.CancelCodecException;
 import us.myles.ViaVersion.exception.CancelDecoderException;
 import us.myles.ViaVersion.util.PipelineUtil;
 
+import java.lang.reflect.InvocationTargetException;
 import java.util.List;
 
 @ChannelHandler.Sharable
 public class FabricDecodeHandler extends MessageToMessageDecoder<ByteBuf> {
     private final UserConnection info;
+    private boolean handledCompression;
+    private boolean skipDoubleTransform;
 
     public FabricDecodeHandler(UserConnection info) {
         this.info = info;
@@ -48,8 +51,15 @@ public class FabricDecodeHandler extends MessageToMessageDecoder<ByteBuf> {
         return info;
     }
 
+    // https://github.com/ViaVersion/ViaVersion/blob/master/velocity/src/main/java/us/myles/ViaVersion/velocity/handlers/VelocityDecodeHandler.java
     @Override
     protected void decode(ChannelHandlerContext ctx, ByteBuf bytebuf, List<Object> out) throws Exception {
+        if (skipDoubleTransform) {
+            skipDoubleTransform = false;
+            out.add(bytebuf.retain());
+            return;
+        }
+
         if (!info.checkIncomingPacket()) throw CancelDecoderException.generate(null);
         if (!info.shouldTransformPacket()) {
             out.add(bytebuf.retain());
@@ -58,11 +68,38 @@ public class FabricDecodeHandler extends MessageToMessageDecoder<ByteBuf> {
 
         ByteBuf transformedBuf = ctx.alloc().buffer().writeBytes(bytebuf);
         try {
+            boolean needsCompress = handleCompressionOrder(ctx, transformedBuf);
+
             info.transformIncoming(transformedBuf, CancelDecoderException::generate);
+
+            if (needsCompress) {
+                CommonTransformer.compress(ctx, transformedBuf);
+                skipDoubleTransform = true;
+            }
             out.add(transformedBuf.retain());
         } finally {
             transformedBuf.release();
         }
+    }
+
+    private boolean handleCompressionOrder(ChannelHandlerContext ctx, ByteBuf buf) throws InvocationTargetException {
+        if (handledCompression) return false;
+
+        int decoderIndex = ctx.pipeline().names().indexOf("decompress");
+        if (decoderIndex == -1) return false;
+        handledCompression = true;
+        if (decoderIndex > ctx.pipeline().names().indexOf("via-decoder")) {
+            // Need to decompress this packet due to bad order
+            CommonTransformer.decompress(ctx, buf);
+            ChannelHandler encoder = ctx.pipeline().get("via-encoder");
+            ChannelHandler decoder = ctx.pipeline().get("via-decoder");
+            ctx.pipeline().remove(encoder);
+            ctx.pipeline().remove(decoder);
+            ctx.pipeline().addAfter("compress", "via-encoder", encoder);
+            ctx.pipeline().addAfter("decompress", "via-decoder", decoder);
+            return true;
+        }
+        return false;
     }
 
     @Override
