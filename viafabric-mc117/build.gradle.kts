cae79c2b68c87946117413d77726fd38afde4e0e
creeper123123321 <7974274+creeper123123321@users.noreply.github.com>
2021-05-02 13:17:19 -0300

    update gradle + loom

--- viafabric-mc117/build.gradle.kts
+++ viafabric-mc117/build.gradle.kts
@@ -3,7 +3,7 @@ version = rootProject.version
 dependencies {
     minecraft("com.mojang:minecraft:21w15a")
     mappings("net.fabricmc:yarn:21w15a+build.16:v2")
-    modCompile("net.fabricmc:fabric-loader:0.10.5+build.213")
+    modImplementation("net.fabricmc:fabric-loader:0.10.5+build.213")
 
     modImplementation("net.fabricmc.fabric-api:fabric-api:0.33.0+1.17")
     modImplementation("com.terraformersmc:modmenu:1.16.9")
