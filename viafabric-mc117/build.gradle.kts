9b21962c301df6649d2012ea93c7b7bb97cc16cd
creeper123123321 <7974274+creeper123123321@users.noreply.github.com>
2021-05-31 17:04:00 -0300

    1.17-pre2

--- viafabric-mc117/build.gradle.kts
+++ viafabric-mc117/build.gradle.kts
@@ -1,8 +1,8 @@
 version = rootProject.version
 
 dependencies {
-    minecraft("com.mojang:minecraft:1.17-pre1")
-    mappings("net.fabricmc:yarn:1.17-pre1+build.9:v2")
+    minecraft("com.mojang:minecraft:1.17-pre2")
+    mappings("net.fabricmc:yarn:1.17-pre2+build.1:v2")
 
     modImplementation("net.fabricmc.fabric-api:fabric-api:0.34.8+1.17")
     modImplementation("com.terraformersmc:modmenu:2.0.0-beta.5")
