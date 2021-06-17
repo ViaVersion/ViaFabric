10cdc366068c10ff1121fb510dd39dcbe7db242b
creeper123123321 <7974274+creeper123123321@users.noreply.github.com>
2021-06-17 15:04:30 -0300

    update deps

--- viafabric-mc117/build.gradle.kts
+++ viafabric-mc117/build.gradle.kts
@@ -2,8 +2,8 @@ version = rootProject.version
 
 dependencies {
     minecraft("com.mojang:minecraft:1.17")
-    mappings("net.fabricmc:yarn:1.17+build.6:v2")
+    mappings("net.fabricmc:yarn:1.17+build.13:v2")
 
-    modImplementation("net.fabricmc.fabric-api:fabric-api:0.34.10+1.17")
-    modImplementation("com.terraformersmc:modmenu:2.0.0-beta.7")
+    modImplementation("net.fabricmc.fabric-api:fabric-api:0.35.2+1.17")
+    modImplementation("com.terraformersmc:modmenu:2.0.2")
 }
