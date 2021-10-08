e2fe610288ccddbbb9e40142e00107912dfb396e
creeper123123321 <7974274+creeper123123321@users.noreply.github.com>
2021-10-08 20:30:57 -0300

    21w40a

--- viafabric-mc118/build.gradle.kts
+++ viafabric-mc118/build.gradle.kts
@@ -1,7 +1,7 @@
 dependencies {
-    minecraft("com.mojang:minecraft:21w38a")
-    mappings("net.fabricmc:yarn:21w38a+build.4:v2")
+    minecraft("com.mojang:minecraft:21w40a")
+    mappings("net.fabricmc:yarn:21w40a+build.1:v2")
 
-    modImplementation("net.fabricmc.fabric-api:fabric-api:0.40.4+1.18")
+    modImplementation("net.fabricmc.fabric-api:fabric-api:0.40.7+1.18")
     modImplementation("com.terraformersmc:modmenu:2.0.2")
 }
