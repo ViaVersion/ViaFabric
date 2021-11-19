8a5982434e368c57a23e671d97f93fef49344aa7
creeper123123321 <7974274+creeper123123321@users.noreply.github.com>
2021-11-19 19:17:36 -0300

    1.18-pre5

--- viafabric-mc118/build.gradle.kts
+++ viafabric-mc118/build.gradle.kts
@@ -1,7 +1,7 @@
 dependencies {
-    minecraft("com.mojang:minecraft:1.18-pre4")
-    mappings("net.fabricmc:yarn:1.18-pre4+build.3:v2")
+    minecraft("com.mojang:minecraft:1.18-pre5")
+    mappings("net.fabricmc:yarn:1.18-pre5+build.2:v2")
 
-    modImplementation("net.fabricmc.fabric-api:fabric-api:0.42.7+1.18")
+    modImplementation("net.fabricmc.fabric-api:fabric-api:0.42.8+1.18")
     modImplementation("com.terraformersmc:modmenu:3.0.0")
 }
