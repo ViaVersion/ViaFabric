ef7062d1eb758c0ee7ce9fd1a67b3d10a892a3f4
creeper123123321 <7974274+creeper123123321@users.noreply.github.com>
2022-01-03 15:18:49 -0300

    update deps

--- viafabric-mc118/build.gradle.kts
+++ viafabric-mc118/build.gradle.kts
@@ -1,7 +1,7 @@
 dependencies {
-    minecraft("com.mojang:minecraft:1.18")
-    mappings("net.fabricmc:yarn:1.18+build.1:v2")
+    minecraft("com.mojang:minecraft:1.18.1")
+    mappings("net.fabricmc:yarn:1.18.1+build.14:v2")
 
-    modImplementation("net.fabricmc.fabric-api:fabric-api:0.43.1+1.18")
+    modImplementation("net.fabricmc.fabric-api:fabric-api:0.45.0+1.18")
     modImplementation("com.terraformersmc:modmenu:3.0.0")
 }
--- viafabric-mc18/build.gradle.kts
+++ viafabric-mc18/build.gradle.kts
@@ -1,13 +1,15 @@
 dependencies {
-	minecraft("com.mojang:minecraft:1.8.9")
-	mappings("net.fabricmc:yarn:1.8.9+build.202112162000:v2") {
-		exclude(group = "net.fabricmc", module = "fabric-loader-1.8.9")
-	}
+    minecraft("com.mojang:minecraft:1.8.9")
+    mappings("net.fabricmc:yarn:1.8.9+build.202112162000:v2")
 
-	modImplementation("net.legacyfabric.legacy-fabric-api:legacy-fabric-api:1.2.0+1.8.9")
-	modImplementation("io.github.boogiemonster1o1:modmenu:0.1.0+1.8.9")
+    modImplementation("net.legacyfabric.legacy-fabric-api:legacy-fabric-api:1.2.0+1.8.9") {
+        exclude(module = "fabric-loader-1.8.9")
+    }
+    modImplementation("io.github.boogiemonster1o1:modmenu:0.1.0+1.8.9") {
+        exclude(module = "fabric-loader-1.8.9")
+    }
 }
 
 loom {
-	intermediaryUrl.set("https://maven.legacyfabric.net/net/fabricmc/intermediary/%1\$s/intermediary-%1\$s-v2.jar")
+    intermediaryUrl.set("https://maven.legacyfabric.net/net/fabricmc/intermediary/%1\$s/intermediary-%1\$s-v2.jar")
 }
