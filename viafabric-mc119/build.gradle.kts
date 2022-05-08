6cb0da6682ebd12d394a5efcc29bc60f2a699c58
creeper123123321 <7974274+creeper123123321@users.noreply.github.com>
2022-05-08 10:04:01 -0300

    update deps, use 4 threads

--- viafabric-mc119/build.gradle.kts
+++ viafabric-mc119/build.gradle.kts
@@ -1,6 +1,6 @@
 dependencies {
-    minecraft("com.mojang:minecraft:22w16b")
-    mappings("net.fabricmc:yarn:22w16b+build.3:v2")
+    minecraft("com.mojang:minecraft:22w18a")
+    mappings("net.fabricmc:yarn:22w18a+build.6:v2")
 
     modImplementation("net.fabricmc.fabric-api:fabric-api:0.51.2+1.19")
     modImplementation("com.terraformersmc:modmenu:3.0.0")
