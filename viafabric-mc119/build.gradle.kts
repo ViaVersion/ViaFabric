491559c0e26b159050ff60cc44b73b443a54559f
creeper123123321 <7974274+creeper123123321@users.noreply.github.com>
2022-06-07 13:34:03 -0300

    1.19

--- viafabric-mc119/build.gradle.kts
+++ viafabric-mc119/build.gradle.kts
@@ -1,7 +1,7 @@
 dependencies {
-    minecraft("com.mojang:minecraft:1.19-rc2")
-    mappings("net.fabricmc:yarn:1.19-rc2+build.1:v2")
+    minecraft("com.mojang:minecraft:1.19")
+    mappings("net.fabricmc:yarn:1.19+build.1:v2")
 
-    modImplementation("net.fabricmc.fabric-api:fabric-api:0.55.0+1.19")
+    modImplementation("net.fabricmc.fabric-api:fabric-api:0.55.1+1.19")
     modImplementation("com.terraformersmc:modmenu:4.0.0-beta.4")
 }
