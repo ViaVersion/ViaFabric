de79985c0338cc381ef59dc678d38deb32cdbf48
FlorianMichael <60033407+FlorianMichael@users.noreply.github.com>
2022-09-18 21:04:40 +0200

    moved Protocol Patching

--- viafabric-mc119/src/main/java/com/viaversion/fabric/mc119/ViaFabric.java
+++ viafabric-mc119/src/main/java/com/viaversion/fabric/mc119/ViaFabric.java
@@ -12,6 +12,7 @@ import com.viaversion.fabric.common.util.JLoggerToLog4j;
 import com.viaversion.fabric.mc119.commands.VRCommandHandler;
 import com.viaversion.fabric.mc119.platform.FabricPlatform;
 import com.viaversion.fabric.mc119.platform.VFLoader;
+import com.viaversion.fabric.mc119.signatures1_19_0.ProtocolPatcher1_19_0;
 import com.viaversion.viaversion.ViaManagerImpl;
 import com.viaversion.viaversion.api.Via;
 import com.viaversion.viaversion.api.data.MappingDataLoader;
@@ -82,6 +83,8 @@ public class ViaFabric implements ModInitializer {
 
         config = new VFConfig(FabricLoader.getInstance().getConfigDir().resolve("ViaFabric")
                 .resolve("viafabric.yml").toFile());
+
+        ProtocolPatcher1_19_0.patch();
 
         INIT_FUTURE.complete(null);
     }
