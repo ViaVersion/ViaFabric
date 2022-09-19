ba38fa2ccbe242b5a592bf876b8d975902307b35
FlorianMichael <60033407+FlorianMichael@users.noreply.github.com>
2022-09-19 02:00:56 +0200

    finally fixed edge-case

--- viafabric-mc119/src/main/java/com/viaversion/fabric/mc119/signatures1_19_0/ProtocolPatcher1_19_0.java
+++ viafabric-mc119/src/main/java/com/viaversion/fabric/mc119/signatures1_19_0/ProtocolPatcher1_19_0.java
@@ -22,7 +22,7 @@ public class ProtocolPatcher1_19_0 {
             public void registerMap() {
                 this.map(Type.STRING);
                 this.map(Type.OPTIONAL_PROFILE_KEY);
-                this.map(Type.OPTIONAL_UUID);
+                this.read(Type.OPTIONAL_UUID);
             }
         }, true);
 
@@ -32,7 +32,7 @@ public class ProtocolPatcher1_19_0 {
             }
         }, true);
 
-        protocol1_19_1To1_19.registerServerbound(State.LOGIN, ClientboundLoginPackets.HELLO.getId(), ClientboundLoginPackets.HELLO.getId(), new PacketRemapper() {
+        protocol1_19_1To1_19.registerServerbound(State.LOGIN, ServerboundLoginPackets.ENCRYPTION_KEY.getId(), ServerboundLoginPackets.ENCRYPTION_KEY.getId(), new PacketRemapper() {
             @Override
             public void registerMap() {
             }
