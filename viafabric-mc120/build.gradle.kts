e3fb5db561a21d9233dfd52bcd3110654174104a
creeper123123321 <7974274+creeper123123321@users.noreply.github.com>
2023-04-08 13:21:09 -0300

    update fabric api

--- viafabric-mc120/build.gradle.kts
+++ viafabric-mc120/build.gradle.kts
@@ -2,7 +2,7 @@ dependencies {
     minecraft("com.mojang:minecraft:23w14a")
     mappings("net.fabricmc:yarn:23w14a+build.4:v2")
 
-    modImplementation("net.fabricmc.fabric-api:fabric-api:0.77.0+1.20")
+    modImplementation("net.fabricmc.fabric-api:fabric-api:0.77.2+1.20")
     modImplementation("com.terraformersmc:modmenu:6.1.0-rc.4")
 }
 
