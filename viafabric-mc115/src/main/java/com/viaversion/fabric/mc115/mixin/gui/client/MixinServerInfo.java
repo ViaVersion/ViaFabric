9654463ac9e2f9083c945276ed8c18aaeebdd4a6
adryd <me@adryd.com>
2023-05-07 17:06:24 -0400

    Show protocol version and advertised version in server listing
    Backport showing protocol version to 1.16, 1.15 and 1.14

---
+++ viafabric-mc115/src/main/java/com/viaversion/fabric/mc115/mixin/gui/client/MixinServerInfo.java
@@ -0,0 +1,30 @@
+package com.viaversion.fabric.mc115.mixin.gui.client;
+
+
+import com.viaversion.fabric.common.gui.ViaServerInfo;
+import net.minecraft.client.network.ServerInfo;
+import org.spongepowered.asm.mixin.Mixin;
+
+@Mixin(ServerInfo.class)
+public class MixinServerInfo implements ViaServerInfo {
+    private boolean viaTranslating;
+    private int viaServerVer;
+
+    public int getViaServerVer() {
+        return viaServerVer;
+    }
+
+    public void setViaServerVer(int viaServerVer) {
+        this.viaServerVer = viaServerVer;
+    }
+
+    @Override
+    public boolean isViaTranslating() {
+        return viaTranslating;
+    }
+
+    @Override
+    public void setViaTranslating(boolean via) {
+        this.viaTranslating = via;
+    }
+}
--- viafabric-mc116/src/main/java/com/viaversion/fabric/mc116/mixin/gui/client/MixinServerEntry.java
+++ viafabric-mc116/src/main/java/com/viaversion/fabric/mc116/mixin/gui/client/MixinServerEntry.java
@@ -1,12 +1,12 @@
-package com.viaversion.fabric.mc118.mixin.gui.client;
+package com.viaversion.fabric.mc116.mixin.gui.client;
 
-import com.mojang.blaze3d.systems.RenderSystem;
 import com.viaversion.fabric.common.gui.ViaServerInfo;
 import com.viaversion.viaversion.api.protocol.version.ProtocolVersion;
 import net.minecraft.client.gui.DrawableHelper;
 import net.minecraft.client.gui.screen.multiplayer.MultiplayerScreen;
 import net.minecraft.client.gui.screen.multiplayer.MultiplayerServerListWidget;
 import net.minecraft.client.network.ServerInfo;
+import net.minecraft.client.texture.TextureManager;
 import net.minecraft.text.Text;
 import net.minecraft.text.TranslatableText;
 import net.minecraft.util.Identifier;
@@ -25,15 +25,13 @@ public class MixinServerEntry {
     @Final
     private ServerInfo server;
 
-    // todo fix this intermediary
-    @Redirect(method = "render", at = @At(value = "INVOKE", ordinal = 0,
-            target = "Lcom/mojang/blaze3d/systems/RenderSystem;setShaderTexture(ILnet/minecraft/class_2960;)V"))
-    private void redirectPingIcon(int i, Identifier identifier) {
+    @Redirect(method = "render", at = @At(value = "INVOKE", ordinal = 0, target = "Lnet/minecraft/client/texture/TextureManager;bindTexture(Lnet/minecraft/util/Identifier;)V"))
+    private void redirectPingIcon(TextureManager textureManager, Identifier identifier) {
         if (identifier.equals(DrawableHelper.GUI_ICONS_TEXTURE) && ((ViaServerInfo) this.server).isViaTranslating()) {
-            RenderSystem.setShaderTexture(i, new Identifier("viafabric:textures/gui/icons.png"));
+            textureManager.bindTexture(new Identifier("viafabric:textures/gui/icons.png"));
             return;
         }
-        RenderSystem.setShaderTexture(i, identifier);
+        textureManager.bindTexture(identifier);
     }
 
     @Redirect(method = "render", at = @At(value = "INVOKE", ordinal = 0, target = "Lnet/minecraft/client/gui/screen/multiplayer/MultiplayerScreen;setTooltip(Ljava/util/List;)V"))
