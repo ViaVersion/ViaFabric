96a70e8393b193a3a108ddc3fc189fb17feef010
Kichura <68134602+Kichura@users.noreply.github.com>
2023-05-10 17:52:17 +0200

    23w18a -> 1.20-pre1.

--- viafabric-mc120/build.gradle.kts
+++ viafabric-mc120/build.gradle.kts
@@ -1,8 +1,8 @@
 dependencies {
-    minecraft("com.mojang:minecraft:23w18a")
-    mappings("net.fabricmc:yarn:23w18a+build.4:v2")
+    minecraft("com.mojang:minecraft:1.20-pre1")
+    mappings("net.fabricmc:yarn:1.20-pre1+build.1:v2")
 
-    modImplementation("net.fabricmc.fabric-api:fabric-api:0.80.0+1.20")
+    modImplementation("net.fabricmc.fabric-api:fabric-api:0.80.2+1.20")
     modImplementation("com.terraformersmc:modmenu:7.0.0-beta.2")
 }
 
