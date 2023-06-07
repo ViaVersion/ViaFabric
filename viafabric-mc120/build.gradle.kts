3369e89d98d2b7d12425caf8039f4d47f75c15a7
Kichura <68134602+Kichura@users.noreply.github.com>
2023-06-08 01:00:32 +0200

    Update Mod Menu to version 7.

--- viafabric-mc120/build.gradle.kts
+++ viafabric-mc120/build.gradle.kts
@@ -3,7 +3,7 @@ dependencies {
     mappings("net.fabricmc:yarn:1.20+build.1:v2")
 
     modImplementation("net.fabricmc.fabric-api:fabric-api:0.83.0+1.20")
-    modImplementation("com.terraformersmc:modmenu:7.0.0-beta.2")
+    modImplementation("com.terraformersmc:modmenu:7.0.0")
 }
 
 tasks.compileJava {
