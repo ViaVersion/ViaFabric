aa797eff1c349ebfaab1e27adc2ef56071e0defd
Kichura <68134602+Kichura@users.noreply.github.com>
2023-06-08 08:48:03 +0200

    Finalize 1.20 for ViaFabric. (sorta) (#252)
    * Final 1.20 for ViaFabric. (Mod Menu isn't updated yet)
    
    * Yarn build 2 of 1.20 doesn't exist yet.
    
    * Cut out the versions from snapshot contexts.
    
    * github why you no work?
    
    * Restore old examples.
    
    * Update Mod Menu to version 7.
    
    * curseforge metadata is weird
    
    ---------
    
    Co-authored-by: creeper123123321 <7974274+creeper123123321@users.noreply.github.com>

--- viafabric-mc120/build.gradle.kts
+++ viafabric-mc120/build.gradle.kts
@@ -1,9 +1,9 @@
 dependencies {
-    minecraft("com.mojang:minecraft:1.20-rc1")
-    mappings("net.fabricmc:yarn:1.20-rc1+build.2:v2")
+    minecraft("com.mojang:minecraft:1.20")
+    mappings("net.fabricmc:yarn:1.20+build.1:v2")
 
     modImplementation("net.fabricmc.fabric-api:fabric-api:0.83.0+1.20")
-    modImplementation("com.terraformersmc:modmenu:7.0.0-beta.2")
+    modImplementation("com.terraformersmc:modmenu:7.0.0")
 }
 
 tasks.compileJava {
