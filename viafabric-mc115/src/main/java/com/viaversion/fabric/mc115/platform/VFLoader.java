c55cd801ec125ea34b2bfedec41aedd0ad9e52dd
EnZaXD <60033407+FlorianMichael@users.noreply.github.com>
2023-06-18 20:43:50 +0200

    Removed MovementTransmitterProvider use() call (updated ViaVersion to 4.7.1-SNAPSHOT) (#259)

--- viafabric-mc115/src/main/java/com/viaversion/fabric/mc115/platform/VFLoader.java
+++ viafabric-mc115/src/main/java/com/viaversion/fabric/mc115/platform/VFLoader.java
@@ -8,15 +8,12 @@ import net.fabricmc.api.EnvType;
 import net.fabricmc.loader.api.FabricLoader;
 import com.viaversion.viaversion.api.Via;
 import com.viaversion.viaversion.api.platform.ViaPlatformLoader;
-import com.viaversion.viaversion.bungee.providers.BungeeMovementTransmitter;
 import com.viaversion.viaversion.api.protocol.version.VersionProvider;
 import com.viaversion.viaversion.protocols.protocol1_9to1_8.providers.HandItemProvider;
-import com.viaversion.viaversion.protocols.protocol1_9to1_8.providers.MovementTransmitterProvider;
 
 public class VFLoader implements ViaPlatformLoader {
     @Override
     public void load() {
-        Via.getManager().getProviders().use(MovementTransmitterProvider.class, new BungeeMovementTransmitter());
         Via.getManager().getProviders().use(VersionProvider.class, new FabricVersionProvider());
 
         if (Via.getPlatform().getConf().isItemCache()) {
