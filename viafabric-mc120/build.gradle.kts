b69dc9baf3a7f634753fa5db9963dc1499f1b1fa
EnZaXD <60033407+FlorianMichael@users.noreply.github.com>
2023-08-03 20:28:31 +0200

    Bump mc 1.20 libraries

--- viafabric-mc120/build.gradle.kts
+++ viafabric-mc120/build.gradle.kts
@@ -1,9 +1,9 @@
 dependencies {
     minecraft("com.mojang:minecraft:1.20.1")
-    mappings("net.fabricmc:yarn:1.20.1+build.2:v2")
+    mappings("net.fabricmc:yarn:1.20.1+build.10:v2")
 
-    modImplementation("net.fabricmc.fabric-api:fabric-api:0.83.1+1.20.1")
-    modImplementation("com.terraformersmc:modmenu:7.1.0")
+    modImplementation("net.fabricmc.fabric-api:fabric-api:0.86.1+1.20.1")
+    modImplementation("com.terraformersmc:modmenu:7.2.1")
 }
 
 tasks.compileJava {
