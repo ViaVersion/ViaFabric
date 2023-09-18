1843cf6a929d0680870b078478baea6cf7ebe584
Kichura <68134602+Kichura@users.noreply.github.com>
2023-09-18 20:49:10 +0200

    1.20.2-rc2, Dependency updates, JDK level correction.

--- viafabric-mc119/build.gradle.kts
+++ viafabric-mc119/build.gradle.kts
@@ -2,8 +2,8 @@ dependencies {
     minecraft("com.mojang:minecraft:1.19.4")
     mappings("net.fabricmc:yarn:1.19.4+build.2:v2")
 
-    modImplementation("net.fabricmc.fabric-api:fabric-api:0.83.0+1.19.4")
-    modImplementation("com.terraformersmc:modmenu:6.2.3")
+    modImplementation("net.fabricmc.fabric-api:fabric-api:0.87.0+1.19.4")
+    modImplementation("com.terraformersmc:modmenu:6.3.1")
 }
 
 tasks.compileJava {
--- viafabric-mc120/build.gradle.kts
+++ viafabric-mc120/build.gradle.kts
@@ -1,9 +1,9 @@
 dependencies {
-    minecraft("com.mojang:minecraft:1.20.2-rc1")
-    mappings("net.fabricmc:yarn:1.20.2-rc1+build.3:v2")
+    minecraft("com.mojang:minecraft:1.20.2-rc2")
+    mappings("net.fabricmc:yarn:1.20.2-rc2+build.4:v2")
 
-    modImplementation("net.fabricmc.fabric-api:fabric-api:0.86.1+1.20.1")
-    modImplementation("com.terraformersmc:modmenu:7.2.1")
+    modImplementation("net.fabricmc.fabric-api:fabric-api:0.89.0+1.20.2")
+    modImplementation("com.terraformersmc:modmenu:8.0.0-beta.2")
 }
 
 tasks.compileJava {
