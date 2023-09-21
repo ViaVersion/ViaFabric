1c563a76826bf1a725a37c33ce69e8630364322e
Kichura <68134602+Kichura@users.noreply.github.com>
2023-09-21 17:52:08 +0200

    1.20.2-rc2 -> 1.20.2.

--- viafabric-mc120/build.gradle.kts
+++ viafabric-mc120/build.gradle.kts
@@ -1,6 +1,6 @@
 dependencies {
-    minecraft("com.mojang:minecraft:1.20.2-rc2")
-    mappings("net.fabricmc:yarn:1.20.2-rc2+build.4:v2")
+    minecraft("com.mojang:minecraft:1.20.2")
+    mappings("net.fabricmc:yarn:1.20.2+build.1:v2")
 
     modImplementation("net.fabricmc.fabric-api:fabric-api:0.89.0+1.20.2")
     modImplementation("com.terraformersmc:modmenu:8.0.0-beta.2")
