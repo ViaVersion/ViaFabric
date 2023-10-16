36a41fd9b48a9a9d8f60992535e313e522eacaa1
FlorianMichael <florian.michael07@gmail.com>
2023-10-16 15:00:44 +0200

    Sync debug mixin to all ViaFabric versions

---
+++ viafabric-mc18/src/main/java/com/viaversion/fabric/mc18/mixin/debug/client/MixinClientConnection.java
@@ -0,0 +1,24 @@
+package com.viaversion.fabric.mc18.mixin.debug.client;
+
+import io.netty.channel.ChannelHandlerContext;
+import net.minecraft.network.ClientConnection;
+import org.apache.logging.log4j.Logger;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(ClientConnection.class)
+public abstract class MixinClientConnection {
+
+    @Shadow
+    @Final
+    private static Logger LOGGER;
+
+    @Inject(method = "exceptionCaught", at = @At("TAIL"))
+    public void exceptionCaught(ChannelHandlerContext context, Throwable ex, CallbackInfo ci) {
+        LOGGER.error("Packet error", ex);
+    }
+}
