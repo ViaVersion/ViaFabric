91a0a1809d20d55d845b5f1b912e5536c3e18545
Kichura <68134602+Kichura@users.noreply.github.com>
2023-12-05 20:35:11 +0100

    Update ViaFabric from 1.20.2->1.20.3 (#284)
    * Prepare to update to 1.20.3. (Hunt for errors first)
    
    * Correct report URL in value, Update Mod Menu.
    
    * Fixed compile errors
    
    ---------
    
    Co-authored-by: FlorianMichael <60033407+FlorianMichael@users.noreply.github.com>

--- viafabric-mc120/build.gradle.kts
+++ viafabric-mc120/build.gradle.kts
@@ -1,9 +1,9 @@
 dependencies {
-    minecraft("com.mojang:minecraft:1.20.2")
-    mappings("net.fabricmc:yarn:1.20.2+build.2:v2")
+    minecraft("com.mojang:minecraft:1.20.3")
+    mappings("net.fabricmc:yarn:1.20.3+build.1:v2")
 
-    modImplementation("net.fabricmc.fabric-api:fabric-api:0.89.3+1.20.2")
-    modImplementation("com.terraformersmc:modmenu:8.0.0")
+    modImplementation("net.fabricmc.fabric-api:fabric-api:0.91.1+1.20.3")
+    modImplementation("com.terraformersmc:modmenu:9.0.0-pre.1")
 }
 
 tasks.compileJava {
