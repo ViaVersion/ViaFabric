9a18c5871683d14d59b5f6f88ccffc0106f8d1a1
Kichura <68134602+Kichura@users.noreply.github.com>
2023-12-05 18:21:36 +0100

    Correct report URL in value, Update Mod Menu.

--- viafabric-mc120/build.gradle.kts
+++ viafabric-mc120/build.gradle.kts
@@ -3,7 +3,7 @@ dependencies {
     mappings("net.fabricmc:yarn:1.20.3+build.1:v2")
 
     modImplementation("net.fabricmc.fabric-api:fabric-api:0.91.1+1.20.3")
-    modImplementation("com.terraformersmc:modmenu:8.0.0")
+    modImplementation("com.terraformersmc:modmenu:9.0.0-pre.1")
 }
 
 tasks.compileJava {
