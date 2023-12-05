f222bbbe0ca9fb0f9ec3e405cdebf8e31a27ada7
Kichura <68134602+Kichura@users.noreply.github.com>
2023-12-05 18:17:47 +0100

    Prepare to update to 1.20.3. (Hunt for errors first)

--- viafabric-mc120/build.gradle.kts
+++ viafabric-mc120/build.gradle.kts
@@ -1,8 +1,8 @@
 dependencies {
-    minecraft("com.mojang:minecraft:1.20.2")
-    mappings("net.fabricmc:yarn:1.20.2+build.2:v2")
+    minecraft("com.mojang:minecraft:1.20.3")
+    mappings("net.fabricmc:yarn:1.20.3+build.1:v2")
 
-    modImplementation("net.fabricmc.fabric-api:fabric-api:0.89.3+1.20.2")
+    modImplementation("net.fabricmc.fabric-api:fabric-api:0.91.1+1.20.3")
     modImplementation("com.terraformersmc:modmenu:8.0.0")
 }
 
