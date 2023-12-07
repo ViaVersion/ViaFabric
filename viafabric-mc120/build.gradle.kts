0cad66de171cc3004546dc88e6bdb23633681dc1
Kichura <68134602+Kichura@users.noreply.github.com>
2023-12-07 20:09:52 +0100

    1.20.3->1.20.4, Update authors list.

--- viafabric-mc120/build.gradle.kts
+++ viafabric-mc120/build.gradle.kts
@@ -1,8 +1,8 @@
 dependencies {
-    minecraft("com.mojang:minecraft:1.20.3")
-    mappings("net.fabricmc:yarn:1.20.3+build.1:v2")
+    minecraft("com.mojang:minecraft:1.20.4")
+    mappings("net.fabricmc:yarn:1.20.4+build.1:v2")
 
-    modImplementation("net.fabricmc.fabric-api:fabric-api:0.91.1+1.20.3")
+    modImplementation("net.fabricmc.fabric-api:fabric-api:0.91.1+1.20.4")
     modImplementation("com.terraformersmc:modmenu:9.0.0-pre.1")
 }
 
