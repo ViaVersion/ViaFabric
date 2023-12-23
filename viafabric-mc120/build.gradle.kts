ce26d061328486f27eb5849ad277ae1464aa74d2
Kichura <68134602+Kichura@users.noreply.github.com>
2023-12-24 00:22:07 +0100

    Update 1.20.4 yarn to build 3.

--- viafabric-mc120/build.gradle.kts
+++ viafabric-mc120/build.gradle.kts
@@ -1,6 +1,6 @@
 dependencies {
     minecraft("com.mojang:minecraft:1.20.4")
-    mappings("net.fabricmc:yarn:1.20.4+build.1:v2")
+    mappings("net.fabricmc:yarn:1.20.4+build.3:v2")
 
     modImplementation("net.fabricmc.fabric-api:fabric-api:0.91.3+1.20.4")
     modImplementation("com.terraformersmc:modmenu:9.0.0")
