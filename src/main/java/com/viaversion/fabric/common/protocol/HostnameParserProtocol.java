08f153f434ad63848b19ece69d4e38ab7fb1875b
FlorianMichael <60033407+FlorianMichael@users.noreply.github.com>
2024-01-21 10:54:24 +0100

    Don't rely on deprecated Via API

--- src/main/java/com/viaversion/fabric/common/protocol/HostnameParserProtocol.java
+++ src/main/java/com/viaversion/fabric/common/protocol/HostnameParserProtocol.java
@@ -3,19 +3,20 @@ package com.viaversion.fabric.common.protocol;
 import com.viaversion.fabric.common.AddressParser;
 import com.viaversion.viaversion.api.protocol.AbstractSimpleProtocol;
 import com.viaversion.viaversion.api.protocol.packet.PacketWrapper;
-import com.viaversion.viaversion.api.protocol.remapper.PacketRemapper;
+import com.viaversion.viaversion.api.protocol.remapper.PacketHandlers;
 import com.viaversion.viaversion.api.protocol.remapper.ValueTransformer;
 import com.viaversion.viaversion.api.type.Type;
 import com.viaversion.viaversion.api.protocol.packet.State;
+import com.viaversion.viaversion.protocols.base.ServerboundHandshakePackets;
 
 public class HostnameParserProtocol extends AbstractSimpleProtocol {
     public static final HostnameParserProtocol INSTANCE = new HostnameParserProtocol();
 
     @Override
     protected void registerPackets() {
-        registerServerbound(State.HANDSHAKE, 0, 0, new PacketRemapper() {
+        registerServerbound(State.HANDSHAKE, ServerboundHandshakePackets.CLIENT_INTENTION.getId(), ServerboundHandshakePackets.CLIENT_INTENTION.getId(), new PacketHandlers() {
             @Override
-            public void registerMap() {
+            protected void register() {
                 map(Type.VAR_INT); // Protocol version
                 map(Type.STRING, new ValueTransformer<String, String>(Type.STRING) {
                     @Override
