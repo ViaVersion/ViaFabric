2aced1f5f5169a8060b23c64e9fca9f3dfb8d29d
FlorianMichael <60033407+FlorianMichael@users.noreply.github.com>
2024-04-21 15:20:34 +0200

    Rename submodules to their explicit mc version
    This is split into several commits so that Git doesn't break completely

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -7,15 +7,15 @@ pluginManagement {
 
 rootProject.name = "ViaFabric"
 
-include("viafabric-mc18")
-include("viafabric-mc112")
-include("viafabric-mc114")
-include("viafabric-mc115")
-include("viafabric-mc116")
-include("viafabric-mc117")
-include("viafabric-mc118")
-include("viafabric-mc119")
-include("viafabric-mc120")
+include("viafabric-mc189")
+include("viafabric-mc1122")
+include("viafabric-mc1144")
+include("viafabric-mc1152")
+include("viafabric-mc1165")
+include("viafabric-mc1171")
+include("viafabric-mc1182")
+include("viafabric-mc1194")
+include("viafabric-mc1204")
 
 plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
