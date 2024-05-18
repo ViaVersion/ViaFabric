00cda57eb51c5d30176fbac97a6c4bd6c9852ab5
Kichura <68134602+Kichura@users.noreply.github.com>
2024-05-18 11:28:00 +0200

    Upstream Mod Menu, Fabric API and Yarn, Add note for CurseGradle.

--- viafabric-mc1206/build.gradle.kts
+++ viafabric-mc1206/build.gradle.kts
@@ -2,7 +2,7 @@ dependencies {
     minecraft("com.mojang:minecraft:1.20.6")
     mappings("net.fabricmc:yarn:1.20.6+build.1:v2")
 
-    modImplementation("net.fabricmc.fabric-api:fabric-api:0.97.8+1.20.6")
+    modImplementation("net.fabricmc.fabric-api:fabric-api:0.98.0+1.20.6")
     modImplementation("com.terraformersmc:modmenu:10.0.0-beta.1")
 }
 
