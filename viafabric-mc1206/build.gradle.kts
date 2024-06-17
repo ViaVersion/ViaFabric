bb316bffa84c9772d4934c08549fb30ba3a31c16
Kichura <68134602+Kichura@users.noreply.github.com>
2024-06-17 18:53:05 +0200

    Upstream deps, Set loader to 0.15.11, Via 5.0.1-SNAPSHOT.

--- viafabric-mc1206/build.gradle.kts
+++ viafabric-mc1206/build.gradle.kts
@@ -1,8 +1,8 @@
 dependencies {
     minecraft("com.mojang:minecraft:1.20.6")
-    mappings("net.fabricmc:yarn:1.20.6+build.1:v2")
+    mappings("net.fabricmc:yarn:1.20.6+build.3:v2")
 
-    modImplementation("net.fabricmc.fabric-api:fabric-api:0.98.0+1.20.6")
+    modImplementation("net.fabricmc.fabric-api:fabric-api:0.100.0+1.20.6")
     modImplementation("com.terraformersmc:modmenu:10.0.0-beta.1")
 }
 
--- viafabric-mc121/build.gradle.kts
+++ viafabric-mc121/build.gradle.kts
@@ -1,9 +1,9 @@
 dependencies {
     minecraft("com.mojang:minecraft:1.21")
-    mappings("net.fabricmc:yarn:1.21+build.1:v2")
+    mappings("net.fabricmc:yarn:1.21+build.2:v2")
 
     modImplementation("net.fabricmc.fabric-api:fabric-api:0.100.1+1.21")
-    modImplementation("com.terraformersmc:modmenu:11.0.0-beta.1")
+    modImplementation("com.terraformersmc:modmenu:11.0.0-rc.4")
 }
 
 tasks.compileJava {
