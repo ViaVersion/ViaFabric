832a29f93502af4087ab3a313c87655ddb94f3f0
creeper123123321 <7974274+creeper123123321@users.noreply.github.com>
2024-07-07 15:09:09 -0300

    remove modmenu integration rewoven modmenu doesn't work anymore

--- viafabric-mc189/build.gradle.kts
+++ viafabric-mc189/build.gradle.kts
@@ -3,9 +3,6 @@ dependencies {
     mappings("net.legacyfabric:yarn:1.8.9+build.541:v2")
 
     modImplementation("net.legacyfabric.legacy-fabric-api:legacy-fabric-api:1.9.4+1.8.9")
-    modImplementation("io.github.boogiemonster1o1:rewoven-modmenu:1.0.0+1.8.9") {
-        isTransitive = false
-    }
 
     // fix newer java
     @Suppress("GradlePackageUpdate", "RedundantSuppression")
