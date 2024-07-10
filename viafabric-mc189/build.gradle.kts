7d71d309bfd3b6178fb1e7d733d63ed9195cf968
creeper123123321 <7974274+creeper123123321@users.noreply.github.com>
2024-07-10 08:33:19 -0300

    use 1.13.2 for common module, update deps

--- viafabric-mc189/build.gradle.kts
+++ viafabric-mc189/build.gradle.kts
@@ -2,7 +2,7 @@ dependencies {
     minecraft("com.mojang:minecraft:1.8.9")
     mappings("net.legacyfabric:yarn:1.8.9+build.541:v2")
 
-    modImplementation("net.legacyfabric.legacy-fabric-api:legacy-fabric-api:1.9.4+1.8.9")
+    modImplementation("net.legacyfabric.legacy-fabric-api:legacy-fabric-api:1.10.2+1.8.9")
 
     // fix newer java
     @Suppress("GradlePackageUpdate", "RedundantSuppression")
