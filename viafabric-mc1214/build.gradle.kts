015ff2ea827941f36da04bbec46aba457d401a38
FlorianMichael <florian.michael07@gmail.com>
2024-12-03 22:53:24 +0100

    1.21.4

---
+++ viafabric-mc1214/build.gradle.kts
@@ -0,0 +1,11 @@
+dependencies {
+    minecraft("com.mojang:minecraft:1.21.4")
+    mappings("net.fabricmc:yarn:1.21.4+build.1:v2")
+
+    modImplementation("net.fabricmc.fabric-api:fabric-api:0.110.5+1.21.4")
+    modImplementation("com.terraformersmc:modmenu:12.0.0-beta.1")
+}
+
+tasks.compileJava {
+    options.release.set(21)
+}
