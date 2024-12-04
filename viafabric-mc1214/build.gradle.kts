7e75589635f2dbf550ecc76aba6d6dba50d16028
Kichura <68134602+Kichura@users.noreply.github.com>
2024-12-04 16:03:49 +0100

    Bump dependencies, remove 1.21.2+1.21.3.

--- viafabric-mc1214/build.gradle.kts
+++ viafabric-mc1214/build.gradle.kts
@@ -3,7 +3,7 @@ dependencies {
     mappings("net.fabricmc:yarn:1.21.4+build.1:v2")
 
     modImplementation("net.fabricmc.fabric-api:fabric-api:0.110.5+1.21.4")
-    modImplementation("com.terraformersmc:modmenu:12.0.0-beta.1")
+    modImplementation("com.terraformersmc:modmenu:13.0.0-beta.1")
 }
 
 tasks.compileJava {
