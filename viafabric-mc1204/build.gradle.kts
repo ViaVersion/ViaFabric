4167fc3b03b268d5e930421dc2087ad9b2b16497
Kichura <68134602+Kichura@users.noreply.github.com>
2024-12-04 16:08:59 +0100

    Update Mod Menu furtherly, Correct metadata in 1.21.4.

--- viafabric-mc1204/build.gradle.kts
+++ viafabric-mc1204/build.gradle.kts
@@ -2,8 +2,8 @@ dependencies {
     minecraft("com.mojang:minecraft:1.20.4")
     mappings("net.fabricmc:yarn:1.20.4+build.3:v2")
 
-    modImplementation("net.fabricmc.fabric-api:fabric-api:0.97.1+1.20.4")
-    modImplementation("com.terraformersmc:modmenu:9.0.0")
+    modImplementation("net.fabricmc.fabric-api:fabric-api:0.97.2+1.20.4")
+    modImplementation("com.terraformersmc:modmenu:9.2.0")
 }
 
 tasks.compileJava {
