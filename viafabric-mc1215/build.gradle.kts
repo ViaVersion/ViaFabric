26c8a93f826970833641583ac515325f20714d44
FlorianMichael <florian.michael07@gmail.com>
2025-03-12 18:52:14 +0100

    1.21.5-pre2

--- viafabric-mc1215/build.gradle.kts
+++ viafabric-mc1215/build.gradle.kts
@@ -1,14 +1,14 @@
 dependencies {
-    minecraft("com.mojang:minecraft:25w09b")
-    mappings("net.fabricmc:yarn:25w09b+build.5:v2")
+    minecraft("com.mojang:minecraft:1.21.5-pre2")
+    mappings("net.fabricmc:yarn:1.21.5-pre2+build.1:v2")
 
-    modImplementation(fabricApi.module("fabric-api-base", "0.118.2+1.21.5"))
-    modImplementation(fabricApi.module("fabric-resource-loader-v0", "0.118.2+1.21.5"))
-    modImplementation(fabricApi.module("fabric-command-api-v1", "0.118.2+1.21.5"))
-    modImplementation(fabricApi.module("fabric-lifecycle-events-v1", "0.118.2+1.21.5"))
-    modImplementation(fabricApi.module("fabric-screen-api-v1", "0.118.2+1.21.5"))
-    modImplementation(fabricApi.module("fabric-registry-sync-v0", "0.118.2+1.21.5"))
-    modImplementation(fabricApi.module("fabric-key-binding-api-v1", "0.118.2+1.21.5"))
+    modImplementation(fabricApi.module("fabric-api-base", "0.118.6+1.21.5"))
+    modImplementation(fabricApi.module("fabric-resource-loader-v0", "0.118.6+1.21.5"))
+    modImplementation(fabricApi.module("fabric-command-api-v1", "0.118.6+1.21.5"))
+    modImplementation(fabricApi.module("fabric-lifecycle-events-v1", "0.118.6+1.21.5"))
+    modImplementation(fabricApi.module("fabric-screen-api-v1", "0.118.6+1.21.5"))
+    modImplementation(fabricApi.module("fabric-registry-sync-v0", "0.118.6+1.21.5"))
+    modImplementation(fabricApi.module("fabric-key-binding-api-v1", "0.118.6+1.21.5"))
     modImplementation("com.terraformersmc:modmenu:13.0.0")
 }
 
