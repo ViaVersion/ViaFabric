d101e9c2736047001afb85814bfe019e93fe51e7
Kichura <68134602+Kichura@users.noreply.github.com>
2025-05-02 15:16:11 +0200

    Check if end-users have git cloned this repository.
    ViaFabric makes use of git-related commands and therefore is required to compile properly.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -18,6 +18,23 @@ include("viafabric-mc1201")
 include("viafabric-mc1206")
 include("viafabric-mc1215")
 
+if (!file(".git").exists()) {
+    val errorText = """
+
+        =====================[ ERROR ]=====================
+         The ViaFabric project directory is not a properly cloned Git repository.
+
+         In order to build ViaFabric from source you must clone
+         the ViaFabric repository using Git, not download a code
+         zip from GitHub.
+
+         Built ViaFabric jars are available for download at
+         https://modrinth.com/mod/viafabric/versions
+        ===================================================
+    """.trimIndent()
+    error(errorText)
+}
+
 plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "0.9.0"
 }
