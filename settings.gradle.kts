5ce016a41483ac28afbce596dc151f253d5df1de
Kichura <68134602+Kichura@users.noreply.github.com>
2025-06-17 22:18:18 +0200

    Update dependencies. (#426)
    Nothing interesting tbh but better than nothing, ViaVersion 5.4.0 isn't finalized which means it stays as-is.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -19,5 +19,5 @@ include("viafabric-mc1206")
 include("viafabric-mc1216")
 
 plugins {
-    id("org.gradle.toolchains.foojay-resolver-convention") version "0.9.0"
+    id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
 }
