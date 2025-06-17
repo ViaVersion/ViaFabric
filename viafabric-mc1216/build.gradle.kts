5e84b36834df9f35ed1b708038e14b769ec79f7a
FlorianMichael <florian.michael07@gmail.com>
2025-06-17 17:18:50 +0200

    1.21.6

--- viafabric-mc1216/build.gradle.kts
+++ viafabric-mc1216/build.gradle.kts
@@ -1,5 +1,5 @@
 dependencies {
-    minecraft("com.mojang:minecraft:1.21.6-rc1")
+    minecraft("com.mojang:minecraft:1.21.6")
     mappings(loom.officialMojangMappings())
 
     modImplementation(fabricApi.module("fabric-api-base", "0.127.0+1.21.6"))
@@ -9,7 +9,7 @@ dependencies {
     modImplementation(fabricApi.module("fabric-screen-api-v1", "0.127.0+1.21.6"))
     modImplementation(fabricApi.module("fabric-registry-sync-v0", "0.127.0+1.21.6"))
     modImplementation(fabricApi.module("fabric-key-binding-api-v1", "0.127.0+1.21.6"))
-    modCompileOnly("com.terraformersmc:modmenu:14.0.0-rc.2") // TODO
+    modCompileOnly("com.terraformersmc:modmenu:15.0.0-beta.1")
 }
 
 tasks.compileJava {
