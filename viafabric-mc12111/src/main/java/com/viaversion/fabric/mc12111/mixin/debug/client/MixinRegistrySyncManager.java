8e3360df681d51f154a3e0e3b953101ad2408a9b
FlorianMichael <florian.michael07@gmail.com>
2025-12-12 01:41:24 +0100

    Update ignore registry sync errors mixin

--- viafabric-mc12111/src/main/java/com/viaversion/fabric/mc12111/mixin/debug/client/MixinRegistrySyncManager.java
+++ viafabric-mc12111/src/main/java/com/viaversion/fabric/mc12111/mixin/debug/client/MixinRegistrySyncManager.java
@@ -21,13 +21,14 @@ import com.viaversion.fabric.mc12111.ViaFabric;
 import it.unimi.dsi.fastutil.objects.Object2IntMap;
 import java.util.Map;
 import net.fabricmc.fabric.impl.registry.sync.RegistrySyncManager;
+import net.minecraft.resources.Identifier;
 import org.slf4j.Logger;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(value = RegistrySyncManager.class, remap = false)
 public class MixinRegistrySyncManager {
@@ -36,12 +37,12 @@ public class MixinRegistrySyncManager {
     @Final
     private static Logger LOGGER;
 
-//    @Inject(method = "checkRemoteRemap", at = @At(value = "INVOKE", target = "Lorg/slf4j/Logger;error(Ljava/lang/String;)V", ordinal = 0), cancellable = true)
-//    private static void ignoreMissingRegistries(Map<ResourceLocation, Object2IntMap<ResourceLocation>> map, CallbackInfo ci) {
-//        if (ViaFabric.config.isIgnoreRegistrySyncErrors()) {
-//            LOGGER.warn("Ignoring missing registries");
-//            ci.cancel();
-//        }
-//    }
+    @Inject(method = "areAllRegistriesOptional", at = @At("HEAD"), cancellable = true)
+    private static void ignoreMissingRegistries(Map<Identifier, Object2IntMap<Identifier>> map, CallbackInfoReturnable<Boolean> cir) {
+        if (ViaFabric.config.isIgnoreRegistrySyncErrors()) {
+            LOGGER.warn("Ignoring missing registries");
+            cir.setReturnValue(true);
+        }
+    }
 
 }
