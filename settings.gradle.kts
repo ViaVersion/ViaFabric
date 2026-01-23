959c9f5ab6bcae042c5487a1d1371f7ab86d1a6a
FlorianMichael <git@florianmichael.de>
2026-01-23 21:34:51 +0100

    26.1-snapshot-4
    Signed-off-by: FlorianMichael <git@florianmichael.de>

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -8,14 +8,7 @@ pluginManagement {
 
 rootProject.name = "ViaFabric"
 
-include("viafabric-mc1144")
-include("viafabric-mc1152")
-include("viafabric-mc1165")
-include("viafabric-mc1171")
-include("viafabric-mc1182")
-include("viafabric-mc1194")
-include("viafabric-mc1206")
-include("viafabric-mc12111")
+include("viafabric-mc26-1")
 
 plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
---
+++ viafabric-mc26-1/src/main/java/com/viaversion/fabric/mixin/debug/client/MixinRegistrySyncManager.java
@@ -0,0 +1,48 @@
+/*
+ * This file is part of ViaFabric - https://github.com/ViaVersion/ViaFabric
+ * Copyright (C) 2018-2026 ViaVersion and contributors
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.viaversion.fabric.mixin.debug.client;
+
+import com.viaversion.fabric.ViaFabric;
+import it.unimi.dsi.fastutil.objects.Object2IntMap;
+import java.util.Map;
+import net.fabricmc.fabric.impl.registry.sync.RegistrySyncManager;
+import net.minecraft.resources.Identifier;
+import org.slf4j.Logger;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(value = RegistrySyncManager.class, remap = false)
+public class MixinRegistrySyncManager {
+
+    @Shadow
+    @Final
+    private static Logger LOGGER;
+
+    @Inject(method = "areAllRegistriesOptional", at = @At("HEAD"), cancellable = true)
+    private static void ignoreMissingRegistries(Map<Identifier, Object2IntMap<Identifier>> map, CallbackInfoReturnable<Boolean> cir) {
+        if (ViaFabric.config.isIgnoreRegistrySyncErrors()) {
+            LOGGER.warn("Ignoring missing registries");
+            cir.setReturnValue(true);
+        }
+    }
+
+}
