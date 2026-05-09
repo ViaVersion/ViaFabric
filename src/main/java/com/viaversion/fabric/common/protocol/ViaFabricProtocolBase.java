c1e12692c21ba4efd5ffd06ba0f3176e8472bb48
Florian Reuth <git@florianreuth.de>
2026-05-09 13:04:58 +0200

    [ci skip] Also check for >= 1.21.5 when comparing

--- src/main/java/com/viaversion/fabric/common/protocol/ViaFabricProtocolBase.java
+++ src/main/java/com/viaversion/fabric/common/protocol/ViaFabricProtocolBase.java
@@ -24,6 +24,7 @@ import com.viaversion.viaversion.api.protocol.AbstractProtocol;
 import com.viaversion.viaversion.api.protocol.packet.ClientboundPacketType;
 import com.viaversion.viaversion.api.protocol.packet.ServerboundPacketType;
 import com.viaversion.viaversion.api.protocol.packet.State;
+import com.viaversion.viaversion.api.protocol.version.ProtocolVersion;
 import com.viaversion.viaversion.api.type.Types;
 import com.viaversion.viaversion.protocols.base.ServerboundHandshakePackets;
 import com.viaversion.viaversion.util.Key;
@@ -57,7 +58,8 @@ public class ViaFabricProtocolBase<CU extends ClientboundPacketType, CM extends 
         // Fixes an issue where the Fabric Particle API causes disconnects when both the client and server have the mod installed and both are 1.21.5+.
         // See https://github.com/ViaVersion/ViaFabric/issues/428
         registerServerbound(customPayload, wrapper -> {
-            if (!wrapper.user().getProtocolInfo().serverProtocolVersion().equals(wrapper.user().getProtocolInfo().protocolVersion())) {
+            final ProtocolVersion serverVersion = wrapper.user().getProtocolInfo().serverProtocolVersion();
+            if (serverVersion.newerThanOrEqualTo(ProtocolVersion.v1_21_5) && !serverVersion.equals(wrapper.user().getProtocolInfo().protocolVersion())) {
                 final String channel = Key.namespaced(wrapper.passthrough(Types.STRING));
                 if (channel.equals("minecraft:register") || channel.equals("minecraft:unregister")) {
                     final List<String> channels = Lists.newArrayList(new String(wrapper.passthrough(Types.SERVERBOUND_CUSTOM_PAYLOAD_DATA), StandardCharsets.UTF_8).split("\0"));
