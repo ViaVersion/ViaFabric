496860355e23083f5b3fb5e599f9514cad39d795
Florian Reuth <git@florianreuth.de>
2026-06-12 04:51:12 +0200

    Add back mod menu integration
    Signed-off-by: Florian Reuth <git@florianreuth.de>

--- viafabric-mc26-1/src/main/java/com/viaversion/fabric/gui/ModMenuConfig.java
+++ viafabric-mc26-1/src/main/java/com/viaversion/fabric/gui/ModMenuConfig.java
@@ -15,22 +15,22 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-//package com.viaversion.fabric.gui;
-//
-//import com.google.common.collect.ImmutableMap;
-//import com.terraformersmc.modmenu.api.ConfigScreenFactory;
-//import com.terraformersmc.modmenu.api.ModMenuApi;
-//import java.util.Map;
-//
-//public class ModMenuConfig implements ModMenuApi {
-//    @Override
-//    public ConfigScreenFactory<?> getModConfigScreenFactory() {
-//        return ViaConfigScreen::new;
-//    }
-//
-//    @Override
-//    public Map<String, ConfigScreenFactory<?>> getProvidedConfigScreenFactories() {
-//        return ImmutableMap.of("viafabric", getModConfigScreenFactory());
-//    }
-//}
+package com.viaversion.fabric.gui;
+
+import com.google.common.collect.ImmutableMap;
+import com.terraformersmc.modmenu.api.ConfigScreenFactory;
+import com.terraformersmc.modmenu.api.ModMenuApi;
+import java.util.Map;
+
+public class ModMenuConfig implements ModMenuApi {
+    @Override
+    public ConfigScreenFactory<?> getModConfigScreenFactory() {
+        return ViaConfigScreen::new;
+    }
+
+    @Override
+    public Map<String, ConfigScreenFactory<?>> getProvidedConfigScreenFactories() {
+        return ImmutableMap.of("viafabric", getModConfigScreenFactory());
+    }
+}
 
