11ade6b0e3e20f7a4a3a688944042728e44d36e5
Florian Reuth <git@florianreuth.de>
2026-06-12 02:41:48 +0200

    26.2-pre-5
    Signed-off-by: Florian Reuth <git@florianreuth.de>

--- viafabric-mc26-2/build.gradle.kts
+++ viafabric-mc26-2/build.gradle.kts
@@ -1,5 +1,5 @@
 dependencies {
-    minecraft("com.mojang:minecraft:26.2-pre-4")
+    minecraft("com.mojang:minecraft:26.2-pre-5")
 
     implementation(fabricApi.module("fabric-api-base", "0.152.0+26.2"))
     implementation(fabricApi.module("fabric-resource-loader-v0", "0.152.0+26.2"))
