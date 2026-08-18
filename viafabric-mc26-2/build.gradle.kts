238c5f46d0853c90e184b0ba465aba34a27a37ec
Florian Reuth <git@florianreuth.de>
2026-08-18 20:05:28 +0200

    26.3-snapshot-9
    Signed-off-by: Florian Reuth <git@florianreuth.de>

--- viafabric-mc26-2/build.gradle.kts
+++ viafabric-mc26-2/build.gradle.kts
@@ -1,11 +1,11 @@
 dependencies {
-    minecraft("com.mojang:minecraft:26.3-snapshot-8")
+    minecraft("com.mojang:minecraft:26.3-snapshot-9")
 
-    implementation(fabricApi.module("fabric-api-base", "0.157.1+26.3"))
-    implementation(fabricApi.module("fabric-resource-loader-v0", "0.157.1+26.3"))
-    implementation(fabricApi.module("fabric-command-api-v2", "0.157.1+26.3"))
-    implementation(fabricApi.module("fabric-lifecycle-events-v1", "0.157.1+26.3"))
-    implementation(fabricApi.module("fabric-screen-api-v1", "0.157.1+26.3"))
-    implementation(fabricApi.module("fabric-registry-sync-v0", "0.157.1+26.3"))
+    implementation(fabricApi.module("fabric-api-base", "0.158.0+26.3"))
+    implementation(fabricApi.module("fabric-resource-loader-v0", "0.158.0+26.3"))
+    implementation(fabricApi.module("fabric-command-api-v2", "0.158.0+26.3"))
+    implementation(fabricApi.module("fabric-lifecycle-events-v1", "0.158.0+26.3"))
+    implementation(fabricApi.module("fabric-screen-api-v1", "0.158.0+26.3"))
+    implementation(fabricApi.module("fabric-registry-sync-v0", "0.158.0+26.3"))
     compileOnly("com.terraformersmc:modmenu:21.0.0-alpha.1")
 }
