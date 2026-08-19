6f4e7d25e20bf813ddb551eed0b55f9188993fdc
Florian Reuth <git@florianreuth.de>
2026-08-19 20:25:34 +0200

    [ci skip] Move plugin versions into settings file
    Signed-off-by: Florian Reuth <git@florianreuth.de>

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,10 @@
 plugins {
     id("java")
     id("maven-publish")
-    id("org.ajoberstar.grgit") version "5.3.2"
-    id("net.fabricmc.fabric-loom") version "1.17-SNAPSHOT"
-    id("com.github.ben-manes.versions") version "0.53.0"
-    id("me.modmuss50.mod-publish-plugin") version "2.2.0"
+    id("org.ajoberstar.grgit")
+    id("net.fabricmc.fabric-loom")
+    id("com.github.ben-manes.versions")
+    id("me.modmuss50.mod-publish-plugin")
 }
 
 private val env = System.getenv()
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,16 +1,23 @@
 pluginManagement {
     repositories {
-        gradlePluginPortal()
         mavenCentral()
+        gradlePluginPortal()
         maven("https://maven.fabricmc.net/")
     }
+
+    plugins {
+        id("org.ajoberstar.grgit") version "5.3.2"
+        id("net.fabricmc.fabric-loom") version "1.17-SNAPSHOT"
+        id("com.github.ben-manes.versions") version "0.53.0"
+        id("me.modmuss50.mod-publish-plugin") version "2.2.0"
+    }
+}
+
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
 }
 
 rootProject.name = "ViaFabric"
 
 include("viafabric-mc26-1")
 include("viafabric-mc26-2")
-
-plugins {
-    id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
-}
